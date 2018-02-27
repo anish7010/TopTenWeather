43623528423c66522a6c3c3653d35975da1afa0f
sanchit <sanchitbathla0609@gmail.com>
2018-02-27 20:06:11 +0530

    Sending files to servlet

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,3 +1,4 @@
+var city,country,temperature,conditions,longitude,latitude;
 function getData() {
 	var name = document.getElementById("name").value;
 	var xhttp = new XMLHttpRequest();
@@ -9,8 +10,13 @@ function getData() {
 			var response = JSON.stringify(result);
 			document.getElementById("content").innerHTML ="City : "+result.name+","+result.sys.country+"<br>" +
 					"Temperature : "+result.main.temp+"<br>" +
-					"Conditions : "+result.weather[0].main+
-					"<button type=\"button\">Add to favorites</button><hr>";
+					"Conditions : "+result.weather[0].main+"<br>"+
+					"Longitude : "+result.coord.lon+"<br>"+
+					"Latitude : "+result.coord.lat+"<br>"+
+					"<button id=\"button\" onclick = \"addFav()\" type = \"button\">Add to favorites</button>";
+				
+			
+					
 		}
 		else{
 			//alert(this.readyState);
@@ -19,3 +25,19 @@ function getData() {
 	xhttp.open("GET", link, true);
 	xhttp.send();
 }
+
+function addFav(){
+	alert("In fav bitch");
+	var xmlhttp = new XMLHttpRequest();
+	
+	xmlhttp.onreadystatechange = function(){
+		if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+			document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+		}
+	};
+	
+	var params = "city=" +city+ "&weather=" +weather+ "&temp=" +temp+ "&min_temp=" +min_temp+ "&max_temp=" +max_temp+ "&wind_speed="+wind_speed;
+	xmlhttp.open('GET',"http://localhost:8080/ClearSky/Favourites?"+params, true);
+	xmlhttp.send();
+}
+
